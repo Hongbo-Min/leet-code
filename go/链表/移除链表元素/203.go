bb26dffd95b1540058d38a401f08c8b78a84ddc7
minhongbo <minhongbo@minieye.cc>
2024-03-28 10:15:52 +0800

    leetcode: 移除链表元素

---
+++ go/链表/移除链表元素/203.go
@@ -0,0 +1,24 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	virtualHead := &ListNode{
+		Next: head,
+	}
+	cur := virtualHead
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return virtualHead.Next
+}
+
+func main() {
+}
