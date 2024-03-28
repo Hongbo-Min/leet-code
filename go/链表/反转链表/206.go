4ee4b6d5ba64aa33bfb5484de7ee098e2d74d8d0
minhongbo <minhongbo@minieye.cc>
2024-03-28 14:25:07 +0800

    leetcode: 反转链表

---
+++ go/链表/反转链表/206.go
@@ -0,0 +1,21 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		temp := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = temp
+	}
+	return pre
+}
+
+func main() {
+}
