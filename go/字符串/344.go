347ebc6990b7326b8f72e9a5741d3d66905d7606
minhongbo <906171472@qq.com>
2024-04-09 19:25:04 +0800

    leetcode: 反转字符串

---
+++ go/字符串/344.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func reverseString(s []byte) {
+	left, right := 0, len(s)-1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
+
+func main() {
+	s := []byte{'h', 'e', 'l', 'l', 'o'}
+	reverseString(s)
+	for _, b := range s {
+		fmt.Print(string(b))
+	}
+}
